3965a0560742d681affac64ec1572e53f767e5d3
Evnek <116468685+Evnek@users.noreply.github.com>
2022-10-22 20:22:17 -0400

    Update twilio_func.py

--- twilio_func.py
+++ twilio_func.py
@@ -1,14 +1,11 @@
-
 from twilio.rest import Client
- 
-account_sid = 'AC213eaf268eb24a22df790ac6ca3ed1bd'
-auth_token = '04de4b2f6ae223388244838150c3019d'
+account_sid = 'AC6dd9388d418f5ffe2371e016ecc2ca0f' # Add your Twilio Account's SID
+auth_token = '0f3399db9ccd000eef8c635de49f6a26' # Add your Twilio Account's Auth Token
 client = Client(account_sid, auth_token)
 def send_rem(date,rem):
-    message = client.messages.create(
-                                  from_='whatsapp:+14155238886',
-                                  body='*REMINDER* '+date+'\n'+rem,
-                                  to='whatsapp:+917696076160'
-                              )
-     
-    print(message.sid)
+  message = client.messages.create(
+  from_='whatsapp:+14155238886',
+  body='*REMINDER* '+date+'\n'+rem,
+  to='whatsapp:+918277164818' # Add your WhatsApp No. here
+)
+print(message.sid) 
